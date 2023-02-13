dfcdaec3a0ccf4487d9d601f91b4d23b709ea958
Valentin Cadart <cadart.valentin@sfr.fr>
2023-02-13 16:02:37 +0100

    Creation of USer entity with security and fixtures with hash password

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,14 +6,22 @@ use Faker\Factory;
 use App\Entity\Product;
 use Liior\Faker\Prices;
 use App\Entity\Category;
+use App\Entity\User;
 use Bezhanov\Faker\Provider\Commerce;
 use Bluemmb\Faker\PicsumPhotosProvider;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    protected $encoder;
+    
+    public function __construct( UserPasswordHasherInterface $encoder){
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager,): void
     {
         $faker = Factory::create('fr_FR');
         $faker->addProvider(new Prices($faker));
@@ -39,6 +47,27 @@ class AppFixtures extends Fixture
         
         }
         
+        
+        
+        $admin = new User();
+        
+        $hash = $this->encoder->hashPassword($admin,"admin");
+        $admin->setFirstName("admin")
+        ->setLastName("admin")
+        ->setEmail("admin@gmail.com")
+        ->setRoles(['ROLE_ADMIN'])
+        ->setPassword($hash);
+        $manager->persist($admin);
+        
+        for ($k=0; $k <5 ; $k++) { 
+            $user = new User();
+            $hash = $this->encoder->hashPassword($user,"password");
+            $user->setEmail("user$k@gmail.com")
+            ->setFirstName($faker->firstName())
+            ->setLastName($faker->lastName())
+            ->setPassword($hash);
+            $manager->persist($user);
+        }
         $manager->flush();
     }
 }
