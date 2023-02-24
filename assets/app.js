6912b5e7908576981890cae6570b7517731a6e1a
Valentin Cadart <cadart.valentin@sfr.fr>
2023-02-24 15:06:33 +0100

    add new component PrivateRoute ans refactoring code

--- assets/app.js
+++ assets/app.js
@@ -1,56 +1,52 @@
-import React, { useContext, useState } from 'react';
-import ReactDOM from 'react-dom';
-import Navbar from './components/Navbar';
-import HomePage from './components/pages/HomePage';
-import './styles/homePage.css';
-import {HashRouter, Switch, Route, withRouter, Redirect} from "react-router-dom";
-import ProductsPage from './components/pages/ProductsPage';
-import CategoriesPage from './components/pages/CategoriesPage';
-import LoginPage from './components/pages/LoginPage';
-import AuthAPI from './components/services/authAPI';
-import AuthContext from './components/contexts/authContext';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/pages/HomePage";
+import "./styles/homePage.css";
+import {
+  HashRouter,
+  Switch,
+  Route,
+  withRouter,
+  Redirect,
+} from "react-router-dom";
+import ProductsPage from "./components/pages/ProductsPage";
+import CategoriesPage from "./components/pages/CategoriesPage";
+import LoginPage from "./components/pages/LoginPage";
+import AuthAPI from "./components/services/authAPI";
+import AuthContext from "./components/contexts/authContext";
+import PrivateRoute from "./components/PrivateRoute";
 
 AuthAPI.setup();
 
-const PrivateRoute = ({path, component}) => {
-
-    const {isAuthenticated} = useContext(AuthContext);
-
-   return isAuthenticated ? (<Route path={path} component={component} />
-    ) : (
-        <Redirect to="/login" />
-    );
- }
-export default PrivateRoute;
-
-
 const App = () => {
-   
-    const [isAuthenticated, setIsAuthenticated] = useState(AuthAPI.isAuthenticated);
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    AuthAPI.isAuthenticated
+  );
 
-    const NavBarWithRouter = withRouter(Navbar);
+  const NavBarWithRouter = withRouter(Navbar);
 
-    const contextValue = {
+  return (
+    <AuthContext.Provider
+      value={{
         isAuthenticated,
-        setIsAuthenticated
-    };
-
-    return (  
-        <AuthContext.Provider value={contextValue}>
-        <HashRouter>
-            <NavBarWithRouter/>
-                <main>
-                    <Switch>
-                        <Route path="/login" component={LoginPage}/>
-                        <PrivateRoute path="/produits" component={ProductsPage} />              
-                        <PrivateRoute path="/categories" component={CategoriesPage}/>
-                        <Route path="/" component={HomePage}/>
-                    </Switch>
-                </main>
-        </HashRouter>
+        setIsAuthenticated,
+      }}
+    >
+      <HashRouter>
+        <NavBarWithRouter />
+        <main>
+          <Switch>
+            <Route path="/login" component={LoginPage} />
+            <PrivateRoute path="/produits" component={ProductsPage} />
+            <PrivateRoute path="/categories" component={CategoriesPage} />
+            <Route path="/" component={HomePage} />
+          </Switch>
+        </main>
+      </HashRouter>
     </AuthContext.Provider>
-    )
-}
+  );
+};
 
-const rootElement = document.querySelector('#app');
+const rootElement = document.querySelector("#app");
 ReactDOM.render(<App />, rootElement);
