18a11c57afa7f214670db8351c290be93235d596
Valentin Cadart <cadart.valentin@sfr.fr>
2023-02-24 08:55:33 +0100

    add privateRoute if authenticated or not

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/pages/HomePage';
 import './styles/homePage.css';
-import {HashRouter, Switch, Route, withRouter} from "react-router-dom";
+import {HashRouter, Switch, Route, withRouter, Redirect} from "react-router-dom";
 import ProductsPage from './components/pages/ProductsPage';
 import CategoriesPage from './components/pages/CategoriesPage';
 import LoginPage from './components/pages/LoginPage';
@@ -11,9 +11,18 @@ import AuthAPI from './components/services/authAPI';
 
 AuthAPI.setup();
 
+const PrivateRoute = ({path, isAuthenticated, component}) => 
+    isAuthenticated ? (<Route path={path} component={component} />
+    ) : (
+        <Redirect to="/login" />
+    );
+ 
+export default PrivateRoute;
+
+
 const App = () => {
    
-    const [isAuthenticated, setIsAuthenticated] = useState(AuthAPI.isAuthenticated);   
+    const [isAuthenticated, setIsAuthenticated] = useState(AuthAPI.isAuthenticated);
 
     const NavBarWithRouter = withRouter(Navbar);
 
@@ -21,10 +30,10 @@ const App = () => {
             <NavBarWithRouter isAuthenticated = {isAuthenticated} onLogout={setIsAuthenticated}/>
                 <main>
                     <Switch>
-                        <Route path="/login" render={(props) => <LoginPage onLogin={setIsAuthenticated} {...props}/>}></Route>
-                        <Route path="/produits" component={ProductsPage}></Route>
-                        <Route path="/categories" component={CategoriesPage}></Route>
-                        <Route path="/" component={HomePage}></Route>
+                        <Route path="/login" render={(props) => <LoginPage onLogin={setIsAuthenticated} {...props}/>}/>
+                        <PrivateRoute path="/produits" isAuthenticated={isAuthenticated} component={ProductsPage} />              
+                        <PrivateRoute path="/categories" isAuthenticated={isAuthenticated} component={CategoriesPage}/>
+                        <Route path="/" component={HomePage}/>
                     </Switch>
                 </main>
     </HashRouter>
